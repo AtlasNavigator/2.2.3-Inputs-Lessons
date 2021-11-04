fb79537a58121422beafbc104c220cabb75b984c
Jose  Prieto <92605951+AtlasNavigator@users.noreply.github.com>
2021-11-04 20:17:35 +0000

    A question

---
+++ index.js
@@ -0,0 +1,4 @@
+// Tells program to get code from this other file, in this case, readline-sync
+const readLine = require('readline-sync');
+let answer = readLine.question("What... what time is it?"); //The question 
+console.log(answer); //The answer
